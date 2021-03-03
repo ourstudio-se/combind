c2164882d65602f9dc94ea002ae08dbaa0768f60
martin bergqlin <martin@ourstudio.se>
2021-03-03 16:33:55 +0100

    added simple tests

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/olivere/elastic/v7 v7.0.22
 	github.com/reveald/reveald v0.0.0-20201127082602-536c61456ca8
 	github.com/sirupsen/logrus v1.8.0
+	github.com/stretchr/testify v1.6.1
 )
---
+++ hash_test.go
@@ -0,0 +1,68 @@
+package combind_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ourstudio-se/combind"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWith2ValuesEquals(t *testing.T) {
+	map1 := map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}
+	map2 := map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}
+
+	h1 := combind.Hash(map1)
+	h2 := combind.Hash(map2)
+	assert.Equal(t, h1, h2)
+}
+
+func TestWithAnyNumberOfValuesEquals(t *testing.T) {
+	map1 := map[string]string{}
+	map2 := map[string]string{}
+
+	for i := 0; i < 1000; i++ {
+		map1[fmt.Sprintf("%d", i)] = fmt.Sprintf("%d", i)
+		map2[fmt.Sprintf("%d", i)] = fmt.Sprintf("%d", i)
+
+	}
+
+	h1 := combind.Hash(map1)
+	h2 := combind.Hash(map2)
+	assert.Equal(t, h1, h2)
+}
+
+func TestNotEqualWhenDifferentNumberOfParams(t *testing.T) {
+	map1 := map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}
+	map2 := map[string]string{
+		"k1": "v1",
+	}
+
+	h1 := combind.Hash(map1)
+	h2 := combind.Hash(map2)
+	assert.NotEqual(t, h1, h2)
+}
+
+func TestNotEqualWhenSameNumberOfParams(t *testing.T) {
+	map1 := map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}
+	map2 := map[string]string{
+		"k1": "v1",
+		"k2": "v22",
+	}
+
+	h1 := combind.Hash(map1)
+	h2 := combind.Hash(map2)
+	assert.NotEqual(t, h1, h2)
+}
